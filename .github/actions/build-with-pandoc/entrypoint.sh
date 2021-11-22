fdf668336e696c8f1c81dd220863c76f4ce51e4c
SUDA Mikihiro <s@sudame.net>
2021-11-22 23:57:04 +0900

    fix: link

--- .github/actions/build-with-pandoc/entrypoint.sh
+++ .github/actions/build-with-pandoc/entrypoint.sh
@@ -6,7 +6,7 @@ cp .github/actions/build-with-pandoc/index.md /tmp/
 for file in markdown/*.md; do
     filename=`basename $file .md`;
     pandoc $file -s -o "docs/$filename.html";
-    echo "- [$filename](/$filename.html) " >> /tmp/index.md
+    echo "- [$filename]($filename.html) " >> /tmp/index.md
 done
 
 pandoc /tmp/index.md -s -o "docs/index.html";
