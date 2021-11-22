01205e3ddf13991d76e89a5f356b03936f14172e
SUDA Mikihiro <s@sudame.net>
2021-11-22 23:51:40 +0900

    add: index

--- .github/actions/build-with-pandoc/entrypoint.sh
+++ .github/actions/build-with-pandoc/entrypoint.sh
@@ -1,6 +1,13 @@
-mkdir -p docs
-for file in "markdown/*.md"
-do
-  filename=`basename $file .md`
-  pandoc $file -s -o "docs/$filename.html"
+rm -rf docs
+mkdir docs
+
+cp .github/actions/build-with-pandoc/index.md /tmp/
+
+for file in markdown/*.md; do
+    filename=`basename $file .md`;
+    pandoc $file -s -o "docs/$filename.html";
+    echo "- [$filename](/$filename.html) " >> /tmp/index.md
 done
+
+pandoc /tmp/index.md -s -o "docs/index.html";
+
